743f3eae0bd8377128b36635b3a44e06450f4230
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-11-03 14:32:54 -0500

    Add a paragraph to the page on each click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var header = document.getElementsByTagName('h1')[0];
 
+
 header.addEventListener('click', function () {
-    
+    var myElement = document.createElement('p');
+    var num = 0;
+    myElement.innerHTML = 'This is click number ' + num;
+    document.getElementsByTagName('body')[0].appendChild(myElement);
 });
