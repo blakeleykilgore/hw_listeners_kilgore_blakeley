74fa1d9517102ae518f1e2bfe4418f44813440a0
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-11-03 15:53:51 -0500

    Add code to print input values to the console

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 var form = document.getElementsByTagName('form')[0];
-
+var firstName = document.getElementById('firstName');
+var lastName = document.getElementById('lastName');
+var email = document.getElementById('email');
+var message = document.getElementById('message');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + email.value);
+    console.log('The message is: ' + message.value);
 });
