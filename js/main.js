366e107c235053c44a365f81a124982656572f8c
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-11-04 19:11:33 -0500

    Make variable num change for each click number

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var header = document.getElementsByTagName('h1')[0];
 
-
-header.addEventListener('click', function () {
+header.addEventListener('click', function (click) {
     var myElement = document.createElement('p');
-    var num = 0;
+    var num = click.detail;
     myElement.innerHTML = 'This is click number ' + num;
     document.getElementsByTagName('body')[0].appendChild(myElement);
 });
