6d6ebfa650eec34a16a508501f75e0538d1f410a
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-06 11:01:44 -0800

    use variable to keep track of clicks

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var header = document.getElementsByTagName('h1')[0];
+var clickTracker = 1;
 
 header.addEventListener('click', function (click) {
     var myElement = document.createElement('p');
     var num = click.detail;
-    myElement.innerHTML = 'This is click number ' + num;
+    myElement.innerHTML = 'This is click number ' + clickTracker;
+    clickTracker = clickTracker + 1;
     document.getElementsByTagName('body')[0].appendChild(myElement);
 });
