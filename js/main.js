7ec8b3c57134c7a405c7c87fcc2cc2b830920d38
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-06 11:02:37 -0800

    remove unnecessary "click" variable

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var header = document.getElementsByTagName('h1')[0];
 var clickTracker = 1;
 
-header.addEventListener('click', function (click) {
+header.addEventListener('click', function () {
     var myElement = document.createElement('p');
-    var num = click.detail;
     myElement.innerHTML = 'This is click number ' + clickTracker;
     clickTracker = clickTracker + 1;
     document.getElementsByTagName('body')[0].appendChild(myElement);
